e71ef8b4c10814496c7f617d8dc708cb93a4789d
Will Reynolds <willreynolds@hoarelea.com>
2020-07-14 02:22:46 +0100

    fixed issues with docker image not connecting

--- api/src/index.js
+++ api/src/index.js
@@ -67,7 +67,7 @@ const driver = neo4j.driver(
   process.env.NEO4J_URI || "bolt://localhost:7687",
   neo4j.auth.basic(
     process.env.NEO4J_USER || "neo4j",
-    process.env.NEO4J_PASSWORD || "neo4j"
+    process.env.NEO4J_PASSWORD || "ne04j"
   ),
   {
     encrypted: process.env.NEO4J_ENCRYPTED ? 'ENCRYPTION_ON' : 'ENCRYPTION_OFF',
